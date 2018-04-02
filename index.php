e9752e89919977390d3fb51b2d97565b05f304db
Sasha Mikhel <mikhel@dumka.pro>
2018-04-02 15:22:40 +0300

    The task manager

--- index.php
+++ index.php
@@ -1,5 +1,16 @@
 <?php
 
 use Todo\Models\Task;
+use Todo\TaskManager;
+use Todo\Storage\MySqlDatabaseTaskStorage;
 
 require 'vendor/autoload.php';
+date_default_timezone_set('Europe/Kiev');
+
+$db = new PDO('mysql:host=127.0.0.1;dbname=todo;' , 'username', 'passwd');
+
+$storage = new MySqlDatabaseTaskStorage($db);
+$manager = new TaskManager($storage);
+
+
+$tasks = $manager->getTasks();
